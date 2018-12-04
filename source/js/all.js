690bb210376a543339627e0a0e8e17b0138cb5f4
AlinaDenisova <denicov_vasya@mail.ru>
2018-12-05 01:49:31 +0400

    Каталог

--- source/js/all.js
+++ source/js/all.js
@@ -163,4 +163,22 @@ $(function() {
   });
 });
 
+'use strict';
+
+$(function() {
+  $(".catalog-card__btn").on("click", function() {
+    var popup = $(this).data("modal");
+    $(popup).show();
+    $(".overlay").addClass("overlay--show");
+  });
+
+  $(".popup").on("click", function(e) {
+    var className = e.target.className;
+    if(className === "popup" || className === "popup__close-btn"){
+      $(this).closest(".popup").hide();
+      $(".overlay").removeClass("overlay--show");
+    }
+  });
+});
+
 //# sourceMappingURL=all.js.map
